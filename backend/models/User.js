e533cc47b095f5de5a2ca1c595772811589529be
Mckayla Bodenstein <mckaylabekker@gmail.com>
2026-06-13 15:24:54 +0200

    Fix backend user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,38 +1,66 @@
-require("dotenv").config()
-
-const express = require("express")
 const mongoose = require("mongoose")
-const cors = require("cors")
+const bcrypt = require("bcrypt")
+const validator = require("validator")
 
-const accommodationRoutes = require("./routes/accommodations")
-const reservationRoutes = require("./routes/reservations")
-const userRoutes = require("./routes/user")
+const Schema = mongoose.Schema
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 })
 
-app.use("/api/accommodations", accommodationRoutes)
-app.use("/api/reservations", reservationRoutes)
-app.use("/api/user", userRoutes)
+userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled")
+  }
 
-app.get("/", (req, res) => {
-  res.json({ message: "Airbnb Capstone API is running" })
-})
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid")
+  }
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("connected to db and listening on port", process.env.PORT)
-    })
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough")
+  }
+
+  const exists = await this.findOne({ email })
+
+  if (exists) {
+    throw Error("Email already in use")
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = await this.create({ email, password: hash })
+
+  return user
+}
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled")
+  }
+
+  const user = await this.findOne({ email })
+
+  if (!user) {
+    throw Error("Incorrect email")
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+
+  if (!match) {
+    throw Error("Incorrect password")
+  }
+
+  return user
+}
+
+module.exports = mongoose.model("User", userSchema)
